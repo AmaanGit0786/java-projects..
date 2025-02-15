f2b70ed88fe165c582654cd86ef2c2e5cbd539d6
AMAAN <mohdamaan0014@gmail.com>
2025-02-15 19:45:17 +0530

    test1

---
+++ IdeaProjects/firstjavaprogram/src/frequent_askedprogram/java/MirrorRightAngleTriangle.java
@@ -0,0 +1,27 @@
+package frequent_askedprogram.java;
+
+import java.util.Scanner;
+
+public class MirrorRightAngleTriangle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value");
+        int n=sc.nextInt();
+        System.out.println("the given right angle mirror pattern pattern are as follows :" );
+
+        for (int i=1;i<=n;i++) {
+
+            for (int j = 1; j <=n-i; j += 1) {
+                System.out.print("   ");
+
+
+            }
+            for (int j = 1; j <= i; j += 1) {
+                System.out.print(" * ");
+
+
+            }
+            System.out.println();
+
+        }
+}}
---
+++ IdeaProjects/firstjavaprogram/src/frequent_askedprogram/java/fullPyramid.java
@@ -0,0 +1,34 @@
+package frequent_askedprogram.java;
+
+import java.util.Scanner;
+
+public class fullPyramid {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value");
+        int n=sc.nextInt();
+        System.out.println("the given pyramid pattern are as follows :" );
+        for (int i=1;i<=n;i++) {
+            for (int j = 1; j <=n-i; j += 1) {
+                System.out.print(" ");
+
+
+            }
+           // System.out.println(" ");
+            for (int j = 1; j <= i; j += 1) {
+                System.out.print("*");
+
+
+            }
+         //   System.out.println(" ");
+            for (int j = 1; j <= i-1; j += 1) {
+                System.out.print("*");
+
+
+            }
+            System.out.println(" ");
+        }
+
+
+    }
+}
